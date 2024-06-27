d01282bd7338e281615948b149ed9d9f918628fe
Alan Lee <aflee@wisc.edu>
2024-06-26 20:33:49 -0500

    Killing the seeding

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,17 +1,17 @@
-const sequelize = require('../config/connection');
-const seedMonsters = require('./monster-seed');
-const seedUsers = require('./user-seed.json');
-const { Monster, User } = require('../models');
-//const seedUsers = require('./userData');
+// const sequelize = require('../config/connection');
+// const seedMonsters = require('./monster-seed');
+// const seedUsers = require('./user-seed.json');
+// const { Monster, User } = require('../models');
+// //const seedUsers = require('./userData');
 
-const seedAll = async () => {
-  await sequelize.sync({ force: true });
+// const seedAll = async () => {
+//   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(seedUsers);
-  await Monster.bulkCreate(seedMonsters);
+//   await User.bulkCreate(seedUsers);
+//   await Monster.bulkCreate(seedMonsters);
 
-  process.exit(0);
-};
+//   process.exit(0);
+// };
 
-module.exports = { seedAll };
+// module.exports = { seedAll };
 
